e81eb78b8510b8dfb91a1a8f467b8c125725ed61
Efren <efrenbarcenas05@gmail.com>
2026-03-10 11:33:01 -0600

    Second commit

--- script.js
+++ script.js
@@ -31,3 +31,39 @@ const observer = new IntersectionObserver(
 revealElements.forEach((element) => {
   observer.observe(element);
 });
+
+
+// Inicializar EmailJS
+emailjs.init("xOD1om8zynV5PPVIF");
+
+// Escuchar el envío del formulario
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("contact-form");
+    const status = document.getElementById("form-status");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const templateParams = {
+            name: document.getElementById("nombre").value,
+            email: document.getElementById("email").value,
+            message: document.getElementById("mensaje").value,
+            title: "Nuevo mensaje desde la web",
+            time: new Date().toLocaleString("es-MX")
+        };
+
+
+        emailjs.send("service_dyotvcd", "template_42464hn", templateParams)
+            .then(function () {
+                status.innerHTML = "Mensaje enviado correctamente.";
+                form.reset();
+            })
+            .catch(function (error) {
+                status.innerHTML = "Error al enviar el mensaje.";
+                console.error("EmailJS error:", error);
+            });
+
+    });
+
+});
